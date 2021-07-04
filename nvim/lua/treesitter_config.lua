c268db2ca5df6af977c53d49f4433d175fe48bd0
Zbigniew Siciarz <zbigniew@siciarz.net>
2021-07-04 21:41:20 +0200

    Map a few treesitter objects to Normal.
    Unfortunately the solarized theme makes these look overwhelmingly blue.

--- nvim/lua/treesitter_config.lua
+++ nvim/lua/treesitter_config.lua
@@ -1,7 +1,15 @@
 require('nvim-treesitter.configs').setup {
     ensure_installed = 'maintained',
     highlight = {
-        enable = true
+        enable = true,
+        -- fix several highlight groups linked to Identifier
+        -- which in solarized is a pretty intense shade of blue
+        -- TODO: do we have to be so blunt? this feels ugly
+        custom_captures = {
+            ['field'] = 'Normal',
+            ['parameter'] = 'Normal',
+            ['variable'] = 'Normal',
+        }
     },
     playground = {
         enable = true
