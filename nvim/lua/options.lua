375c4b279930ecdb4cfeb8dd2eb9d424d577c3aa
Zbigniew Siciarz <zbigniew@siciarz.net>
2021-07-04 16:36:12 +0200

    Start porting vim config to neovim-flavoured Lua.

---
+++ nvim/lua/options.lua
@@ -0,0 +1,55 @@
+local opt = vim.opt
+
+-- highlight current line
+opt.cursorline = true
+-- show matching bracket
+opt.showmatch = true
+-- disable highlighting of search results
+opt.hlsearch = false
+-- if ignorecase is set, detect uppercase chars in search pattern
+opt.smartcase = true
+-- infer correct character case upon word completion
+opt.infercase = true
+-- show line numbers
+opt.number = true
+-- minimal number of columns for line number
+opt.numberwidth = 5
+-- automatically change directory when opening a file
+opt.autochdir = true
+-- enable mouse
+opt.mouse = 'a'
+
+-- ignore some (binary and whatnot) files and directories
+opt.wildignore = '*/htmlcov*,*/build/*,*/dist/*,*/attachments/*,*/node_modules*,*/elm-stuff/*,*/target/*,*.so,*.o,*.pyc,*.pyo,*.swp,*.jar,*.hi'
+-- don't fold code (personal preference)
+opt.foldenable = false
+-- copy backup files instead of moving; fixes filesystem watchers
+opt.backupcopy = 'yes'
+-- enable switching between unsaved buffers
+opt.hidden = true
+
+-- Colors
+-- ======
+-- enable 24-bit color
+opt.termguicolors = true
+-- enable Solarized
+vim.cmd('colorscheme solarized')
+-- print margin
+opt.colorcolumn = '80'
+
+-- Indentation
+-- ===========
+-- indent 4 spaces in a consistent manner
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
+-- round indent to multiple of shiftwidth
+opt.shiftround = true
+-- expand tabs to spaces
+opt.expandtab = true
+-- higlight tabs for indentation (Solarized-compatible colors)
+opt.list = true
+opt.listchars = 'tab:▸ ,eol: '
+
+-- use filler lines and open diff in vertical splits
+opt.diffopt = 'filler,vertical'
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,32 @@
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+-- bootstrap as in packer README
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
+
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  -- Improvements to vim commands and motions
+  use 'tpope/vim-repeat'
+  use 'tpope/vim-surround'
+  use 'tpope/vim-unimpaired'
+  use 'michaeljsmith/vim-indent-object'
+  -- 'vim as IDE' features
+  use 'preservim/nerdcommenter'
+  use 'preservim/tagbar'
+  use 'FooSoft/vim-argwrap'
+  use 'vim-airline/vim-airline'
+  use 'vim-airline/vim-airline-themes'
+  -- TODO: 'edkolev/tmuxline.vim'
+  use 'vim-test/vim-test'
+  use 'hrsh7th/nvim-compe'
+  -- Color scheme
+  use 'ishan9299/nvim-solarized-lua'
+  -- Python plugins
+  use 'raimon49/requirements.txt.vim'
+  use {'psf/black',  tag = '20.8b1' }
+end)
