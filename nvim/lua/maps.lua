b73150893a59ed740b8342a5faf7aa86e07b67df
Zbigniew Siciarz <zbigniew@siciarz.net>
2022-01-25 20:21:53 +0100

    Remove remaining vim-compe config

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -15,11 +15,3 @@ map('', '<F8>', ':TagbarToggle<CR>', {noremap = true})
 
 -- \t to run a single test nearest to the cursor
 map('n', '<leader>t', ':TestNearest<CR>', {noremap = true, silent = true})
-
--- open autocomplete on Ctrl+Space
-map('i', '<C-Space>', 'compe#complete()', {noremap = true, silent = true, expr = true})
-map('i', '<CR>', 'compe#confirm("<CR>")', {noremap = true, silent = true, expr = true})
--- use Tab/Shift+Tab to scroll between suggestions
-map('i', '<TAB>', 'pumvisible() ? "\\<C-n>" : "\\<TAB>"', {noremap = true, silent = true, expr = true})
-map('i', '<S-TAB>', 'pumvisible() ? "\\<C-p>" : "\\<C-h>"', {noremap = true, silent = true, expr = true})
--- this may need expanded version like https://github.com/hrsh7th/nvim-compe#how-to-use-tab-to-navigate-completion-menu
