3b178e3c727b78cbf1b2b52e0a29a4a230b31254
Zbigniew Siciarz <zbigniew@siciarz.net>
2022-12-23 10:17:21 +0100

    Use default_capabilities() instead of deprecated CMP API

--- nvim/lua/lsp_config.lua
+++ nvim/lua/lsp_config.lua
@@ -14,8 +14,7 @@ local on_attach = function(client, bufnr)
   vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 lspconfig.pylsp.setup {
   on_attach = on_attach,
