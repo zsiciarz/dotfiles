f1d8acd3f6e58babe131d662ee6deab8094b526f
Zbigniew Siciarz <zbigniew@siciarz.net>
2023-12-25 18:19:36 +0100

    Modernize LSP config.

--- nvim/lua/lsp_config.lua
+++ nvim/lua/lsp_config.lua
@@ -3,47 +3,35 @@ local null_ls = require 'null-ls'
 
 local opts = { noremap=true, silent=true }
 
-vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 
-local on_attach = function(client, bufnr)
-  -- TODO: more key bindings as per https://github.com/neovim/nvim-lspconfig
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>d', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
 
-  -- run formatters before save
-  vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-end
+local format_group = vim.api.nvim_create_augroup("LspFormatting", {})
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-lspconfig.pylsp.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-
-lspconfig.tsserver.setup {
-  on_attach = function(client, bufnr)
-    client.resolved_capabilities.document_formatting = false
-    client.resolved_capabilities.document_range_formatting = false
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>d', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', '<leader>d', vim.lsp.buf.definition, opts)
+    vim.api.nvim_clear_autocmds({ group = format_group, buffer = ev.buf })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = format_group,
+      buffer = ev.buf,
+      callback = function()
+        vim.lsp.buf.format { async = false }
+      end,
+    })
   end,
-  capabilities = capabilities,
-  -- TODO: disable tsserver formatting and use prettier
-  -- see: https://jose-elias-alvarez.medium.com/configuring-neovims-lsp-client-for-typescript-development-5789d58ea9c
-  -- and https://github.com/MunifTanjim/prettier.nvim
-}
+})
 
-lspconfig.jsonls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-
+lspconfig.pylsp.setup {}
+lspconfig.tsserver.setup {}
+lspconfig.jsonls.setup {}
 null_ls.setup {
   sources = {
     null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.code_actions.eslint,
     null_ls.builtins.formatting.prettier,
   },
-  on_attach = on_attach,
 }
